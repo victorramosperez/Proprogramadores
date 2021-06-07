b62870107c18f4535f6162bf2f45c4b769accc4e
rpleiter <47854605+rpleiter@users.noreply.github.com>
2021-06-07 15:47:00 +0200

    Pruebas de dibujo de enemigo

--- src/Enemigo.cpp
+++ src/Enemigo.cpp
@@ -5,6 +5,11 @@ Enemigo::Enemigo()
 	Direccion = 0;
 	Vivo = true;
 	Vida = 1;
+    	aceleracion.x = aceleracion.y = 0;
+    	Altura = 1.8f;
+   	sprite.setCenter(1, 0);
+    	sprite.setSize(2, 2);
+   
 }
 
 Enemigo::~Enemigo()
@@ -14,13 +19,18 @@ Enemigo::~Enemigo()
 
 void Enemigo::dibuja()
 {
-	/*
-	glColor3ub(255, 255, 255);
-	glBegin(GL_POLYGON);
-	glVertex3f();
-	glVertex3f();
-	glVertex3f();
-	glVertex3f();
-	glEnd();
-	*/
+	glPushMatrix();
+    glTranslatef(posicion.x, posicion.y, 10);
+    glColor3f(1.0f, 0.0f, 0.0f);
+    //glutSolidSphere(Altura, 20, 20);
+    //gestion de direccion y animacion
+    if (velocidad.x > 0.01)sprite.flip(false, false);
+    if (velocidad.x < -0.01)sprite.flip(true, false);
+    if ((velocidad.x < 0.01) && (velocidad.x > -0.01))
+        sprite.setState(0);
+    else if (sprite.getState() == 0)
+        sprite.setState(1, false);
+    sprite.draw();
+    glPopMatrix();
+	
 }
