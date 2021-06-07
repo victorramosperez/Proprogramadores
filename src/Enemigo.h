8e245c7366861a4132ed924d103e19a816fa2208
rpleiter <47854605+rpleiter@users.noreply.github.com>
2021-06-07 15:48:36 +0200

    Prueba dibujo enemigo

--- src/Enemigo.h
+++ src/Enemigo.h
@@ -1,6 +1,9 @@
 #pragma once
 #include "ObjetoMovil.h"
+#include "ETSIDI.h"
 
+
+using ETSIDI::SpriteSequence;
 class Enemigo : public ObjetoMovil
 {
 protected:
@@ -8,19 +11,22 @@ protected:
     int Direccion;
     int Vida;
     bool Vivo;
-
+    float Altura;
 public:
     Enemigo();
     ~Enemigo();
     void dibuja();
     void setDireccion(int ndir) { Direccion = ndir; }
     int getDireccion() { return Direccion; }
+    
+    SpriteSequence sprite{ "imagenes/zoomer.png", 5 };
+    friend class Interaccion;
+    
 };
-
 
 class EnemigoSimple : public Enemigo { 
 public:
-	EnemigoSimple(Vector2D posicion);
+	//EnemigoSimple(Vector2D posicion);
 protected:
 	//sprite
 };
